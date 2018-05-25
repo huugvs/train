c447fe60638902178b685bf8e999943b3bd67680
Hugo <itsmehugo98@gmail.com>
2018-05-25 10:24:28 +0200

    Added entity

---
+++ src/AppBundle/Entity/gebruikers/name.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity\gebruikers;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * name
+ *
+ * @ORM\Table(name="gebruikersname")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\gebruikers\nameRepository")
+ */
+class name
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
